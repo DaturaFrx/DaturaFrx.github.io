27ae175c3913d079f34a305e9d9352701bf7ddf5
Datura <66500636+DaturaFrx@users.noreply.github.com>
2024-11-23 15:02:28 -0800

    Delete js directory

--- js/logs.js
+++
@@ -1,7 +0,0 @@
-function log(message) {
-    const debugArea = document.getElementById('debugArea');
-    const timestamp = new Date().toLocaleTimeString();
-    debugArea.innerHTML += `<div>[${timestamp}] ${message}</div>`;
-    debugArea.scrollTop = debugArea.scrollHeight;
-    console.log(`[${timestamp}] ${message}`);
-}
--- js/other/toastNotification.js
+++
@@ -1,9 +0,0 @@
-function showToast() {
-    const toast = document.getElementById('toast');
-    toast.classList.add('show');
-
-    // Hide the toast after 3 seconds
-    setTimeout(() => {
-        toast.classList.remove('show');
-    }, 3000);
-}
